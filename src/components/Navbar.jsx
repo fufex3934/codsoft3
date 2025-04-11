ef266e0477571ecbdbcbff225ed0a8c9f0640357
fufex3934 <fufawakgari174@gmail.com>
2025-04-11 13:54:12 +0300

    adding navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/create'}>Create Post</Link>
+    </nav>
   )
 }
