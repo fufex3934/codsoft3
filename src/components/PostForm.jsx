d31e177fc4ceb8131ee629354702c8f023524978
fufex3934 <fufawakgari174@gmail.com>
2025-04-11 14:01:11 +0300

    add pages

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function PostForm() {
+    const [title,setTitle ] = useState("");
+    const [content,setContent] = useState("");
   return (
-    <div>PostForm</div>
+    <form>
+        <input type="text"
+        placeholder='Title'
+        value={title}
+        onChange={(e)=>setTitle(e.target.value)}
+         />
+         <textarea 
+         value={content}
+         placeholder='Content'
+         onChange={(e)=>setContent(e.target.value)}
+         />
+         <button type='submit'>Post</button>
+    </form>
   )
 }
